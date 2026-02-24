1520313b419f773ec3aa393d67b7472e63d8269d
weenie <59868100+weeniemount@users.noreply.github.com>
2026-02-24 10:36:20 +0100

    add missing dependencies for chromiumos

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux unzip
+dnf5 install -y tmux unzip nss nspr cups-libs
 
 # Use a COPR Example:
 #
